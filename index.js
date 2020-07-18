b0a53f7db39d729cf178ef2f97870b1ed6ac3ba0
Mong Chang Hsi <m.changhsi@gmail.com>
2020-07-18 11:15:21 +0800

    Parsing DB

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const folder = require('./routes/folders');
 const cors = require('cors');
 const config = require('config')
 
-if (!config.get('db')){
+const stringFromDB = config.get('db')
+
+if (!stringFromDB){
     console.error('config in db is not defined');
     process.exit(1);
 }
 
-const client = new MongoClient(config.get('db'), { useNewUrlParser: true });
+// const client = new MongoClient(stringFromDB, { useNewUrlParser: true });
 
-mongoose.connect(client)
+mongoose.connect(stringFromDB, { useNewUrlParser: true })
     .then(() => console.log('Connected to Mongodb...'))
     .catch((error) => console.error('Error connecting to Mongodb...', error.message))
 
